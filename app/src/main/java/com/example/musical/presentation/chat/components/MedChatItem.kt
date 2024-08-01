5944e0739d23aa49539508b240b9b83078ec7986
SahilKumar75 <sahilkumargreat12@gmail.com>
2024-08-01 14:10:20 +0530

    fitness updated

--- app/src/main/java/com/example/musical/presentation/chat/components/MedChatItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.musical.presentation.chat.components
-
-data class MedChatItem(
-    val id: Int,
-    val name: String,
-    val lastMessage: String,
-    val timestamp: String
-)
